ac7050ab139edd9d535673d598aa9021366891e0
Leonardo Albergoni Viana <48710470832@>
2025-01-21 16:51:06 -0300

    django att

--- api/views.py
+++ api/views.py
@@ -3,7 +3,8 @@ from .models import Cadastro
 from .serializer import CadastroSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def listar_professores(request):
@@ -11,7 +12,20 @@ def listar_professores(request):
         queryset = Cadastro.objects.all()
         serializer = CadastroSerializer(queryset, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CadastroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+# vizualiza todos os cadastros feitos e cadastra novos campos
 class ProfessoresView(ListCreateAPIView):
+    queryset = Cadastro.objects.all()
+    serializer_class = CadastroSerializer
+
+# Classe que retorna um cadastro feito, podendo fazer o update e o delete tambem
+class ProfessoresDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Cadastro.objects.all()
     serializer_class = CadastroSerializer
