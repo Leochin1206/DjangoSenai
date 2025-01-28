716e24d18b8cf9c47e18383f04a2f285d927b5e6
Leonardo Albergoni Viana <48710470832@>
2025-01-28 16:57:51 -0300

    280125

---
+++ Front/src/App.jsx
@@ -0,0 +1,20 @@
+import React  from 'react';
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+    <div className='container'>
+      <h1>Login</h1>
+
+      <input className='caixa' value={""} placeholder='User'/>
+
+      <input className='caixa' value={""} placeholder='Password' type="password"/>
+
+      <button className='btn'>Ok</button>
+    </div>
+    </>
+  )
+}
+
+
